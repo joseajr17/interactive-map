722371f413634517f6f99ca84d7f0e5efa600157
jose <josejr017@gmail.com>
2025-10-02 15:45:52 -0300

    feat: changing flex-direction of <MapLegend />

--- src/components/MapLegend/index.tsx
+++ src/components/MapLegend/index.tsx
@@ -3,20 +3,15 @@ type MapLegendProps = {
 };
 
 export function MapLegend({ statusColors }: MapLegendProps) {
-  // Dados da legenda
   const legendItems = [
-    {
-      id: "proposta_e_promulgada",
-      label: "Proposta e Promulgada",
-      color: statusColors.proposta_e_promulgada,
-    },
+    { id: "proposta_e_promulgada", label: "Proposta e Promulgada", color: statusColors.proposta_e_promulgada },
     { id: "promulgada", label: "Promulgada", color: statusColors.promulgada },
     { id: "proposta", label: "Proposta", color: statusColors.proposta },
     { id: "nenhuma", label: "Nenhuma Lei", color: statusColors.nenhuma },
   ];
 
   return (
-    <div className="flex flex-wrap justify-center gap-4 p-4 bg-white rounded-lg shadow-md">
+    <div className="flex flex-col items-start gap-2 p-4 bg-white rounded-lg shadow-md">
       {legendItems.map((item) => (
         <div key={item.id} className="flex items-center gap-2">
           <div
