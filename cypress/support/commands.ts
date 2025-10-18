29442b17ca3414e5c01b203b744158c0c737e9fe
jose <josejr017@gmail.com>
2025-10-17 21:09:11 -0300

    refactor: using cy.window() in viewport commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,27 +1,30 @@
-Cypress.Commands.add('isInViewport', { prevSubject: true }, (subject) => {
-  const window = cy.state('window');
-  const bottom = window.innerHeight;
-  const right = window.innerWidth;
-  const rect = subject[0].getBoundingClientRect();
+Cypress.Commands.add("isInViewport", { prevSubject: true }, (subject) => {
+  // cy.window() obtém o objeto 'window' de forma segura
+  cy.window().then((win) => {
+    const bottom = win.innerHeight;
+    const right = win.innerWidth;
+    const rect = subject[0].getBoundingClientRect();
 
-  expect(rect.top).to.be.within(0, bottom);
-  expect(rect.bottom).to.be.within(0, bottom);
-  expect(rect.left).to.be.within(0, right);
-  expect(rect.right).to.be.within(0, right);
+    expect(rect.top).to.be.within(0, bottom);
+    expect(rect.bottom).to.be.within(0, bottom);
+    expect(rect.left).to.be.within(0, right);
+    expect(rect.right).to.be.within(0, right);
+  });
 
+  // Retorna o 'subject' original para permitir que outros comandos sejam encadeados
+  // Ex: cy.get('...').isInViewport().click();
   return subject;
 });
 
-Cypress.Commands.add('isNotInViewport', { prevSubject: true }, (subject) => {
-  const window = cy.state('window');
-  const bottom = window.innerHeight;
-  const rect = subject[0].getBoundingClientRect();
+Cypress.Commands.add("isNotInViewport", { prevSubject: true }, (subject) => {
+  cy.window().then((win) => {
+    const bottom = win.innerHeight;
+    const rect = subject[0].getBoundingClientRect();
+    const isAbove = rect.bottom < 0;
+    const isBelow = rect.top > bottom;
 
-  // Verifica se o elemento está completamente acima ou completamente abaixo da viewport
-  const isAbove = rect.bottom < 0;
-  const isBelow = rect.top > bottom;
+    expect(isAbove || isBelow).to.be.true;
+  });
 
-  expect(isAbove || isBelow).to.be.true;
-  
   return subject;
 });
